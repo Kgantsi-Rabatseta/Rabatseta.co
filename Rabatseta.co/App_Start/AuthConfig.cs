91678b508cb5a0808754ad6c8f8f3bfd5bd12152
Kgantsi Rabatseta <kgantsi.rabatseta@deposita.co.za>
2016-01-26 23:16:06 +0200

    Add References for OAuth

---
+++ Rabatseta.co/App_Start/AuthConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Web.WebPages.OAuth;
+
+namespace Rabatseta.co.App_Start
+{
+    public class AuthConfig
+    {
+        //This class defines all authentications that are possible
+        public static void RegisterAuthentication()
+        {
+            OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
---
+++ Rabatseta.co/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Web.WebPages.OAuth;
+
+namespace Rabatseta.co.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult LoginPage()
+        {
+            return View(OAuthWebSecurity.RegisteredClientData);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public void ExternalLogin(string provider, string returnUrl)
+        {
+            OAuthWebSecurity.RequestAuthentication(provider, Url.Action("ExternalLoginCallback", new { ReturnUrl = returnUrl }));
+        }
+    }
+}
